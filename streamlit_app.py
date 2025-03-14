b2672cff79d1fb4d6a470eed04ea3587772f8b30
gunasekaran81m <gunasekaran81m@gmail.com>
2025-03-14 15:22:38 +0530

    Update streamlit_app.py
    df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -28,7 +29,9 @@ if ingredients_list:
    ingredients_string = ''
 
    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '      
+        ingredients_string += fruit_chosen + ' '   
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
         #st.write(ingredients_string) 
 
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
@@ -40,8 +43,5 @@ if ingredients_list:
       session.sql(my_insert_stmt).collect()
       st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
+
     
